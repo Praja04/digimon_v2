8530fba8b83f032404df08805e1d707c3c3d4a6c
reishantridyarafly <reishantridyarafly@gmail.com>
2026-03-13 15:53:42 +0700

    chore(seeder): update monitoring turun blending seeder data

--- database/seeders/MonitoringTurunBlendingSeeder.php
+++ database/seeders/MonitoringTurunBlendingSeeder.php
@@ -9,7 +9,7 @@ class MonitoringTurunBlendingSeeder extends Seeder
 {
     public function run(): void
     {
-        $productionBatchId = 39;
+        $productionBatchId = 28;
         $qcUserId = 4;
 
         $batch = DB::table('production_batches')
@@ -29,38 +29,32 @@ class MonitoringTurunBlendingSeeder extends Seeder
         foreach ($rows as $id) {
             $brix = 76;
             $visco = 18;
-            $nacl = 6.2;
 
             switch ($variant) {
 
                 case 'MSD':
                     $brix = rand(7400, 7600) / 100;
                     $visco = rand(700, 1000) / 100;
-                    $nacl = rand(1150, 1450) / 100;
                     break;
 
                 case 'JB':
                     $brix = rand(7600, 8000) / 100;
                     $visco = rand(1600, 2500) / 100;
-                    $nacl = rand(600, 630) / 100;
                     break;
 
                 case 'SS1':
                     $brix = rand(7500, 7900) / 100;
                     $visco = rand(1400, 2200) / 100;
-                    $nacl = rand(630, 650) / 100;
                     break;
 
                 case 'SS2':
                     $brix = rand(7500, 7900) / 100;
                     $visco = rand(1600, 2400) / 100;
-                    $nacl = rand(630, 650) / 100;
                     break;
 
                 case 'BB':
                     $brix = rand(7700, 8100) / 100;
                     $visco = rand(1700, 2800) / 100;
-                    $nacl = rand(600, 630) / 100;
                     break;
             }
 
@@ -73,10 +67,8 @@ class MonitoringTurunBlendingSeeder extends Seeder
                 'created_by'  => $qcUserId,
                 'scanned_at'  => now(),
                 'brix'        => $brix,
-                'nacl'        => $nacl,
                 'visco'       => $visco,
                 'aw'          => $aw,
-                'organo'      => 'OK',
                 'shift'       => 1,
             ]);
         }
