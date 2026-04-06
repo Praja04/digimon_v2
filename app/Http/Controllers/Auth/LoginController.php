65f2b232653612b93903245b4e71f53fc9575096
reishantridyarafly <reishantridyarafly@gmail.com>
2026-04-06 15:40:25 +0700

    refactor(auth): change post-login redirect path to '/halaman-utama'

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/halaman-utama';
 
     /**
      * Create a new controller instance.
